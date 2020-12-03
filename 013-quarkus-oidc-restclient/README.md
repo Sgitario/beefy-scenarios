217e046a6ba4fbc97623ec8dba983a4de987b422
Jose <josecarvajalhilario@gmail.com>
2020-12-03 14:06:45 +0100

    Added module OIDC with RestClient

---
+++ 013-quarkus-oidc-restclient/README.md
@@ -0,0 +1,4 @@
+Module that covers the RestClient and OIDC integration using Keycloak container.
+There are two applications:
+- Ping application that will invoke the Pong application using the RestClient and will return the expected "ping pong" output.
+- Pong application that will return the "pong" output.
---
+++ 013-quarkus-oidc-restclient/src/test/java/io/quarkus/qe/AbstractPingPongResourceTest.java
@@ -0,0 +1,66 @@
+package io.quarkus.qe;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.Test;
+import org.keycloak.authorization.client.AuthzClient;
+
+import io.quarkus.qe.containers.KeycloakTestResource;
+import io.quarkus.test.common.QuarkusTestResource;
+
+@QuarkusTestResource(KeycloakTestResource.class)
+public abstract class AbstractPingPongResourceTest {
+
+    private static final String PING_ENDPOINT = "/%s-ping";
+    private static final String PONG_ENDPOINT = "/%s-pong";
+    private static final String USER = "test-user";
+
+    AuthzClient authzClient;
+
+    @Test
+    public void testPingUnauthorized() {
+        given()
+                .when().get(pingEndpoint())
+                .then().statusCode(HttpStatus.SC_UNAUTHORIZED);
+    }
+
+    @Test
+    public void testPongUnauthorized() {
+        given()
+                .when().get(pongEndpoint())
+                .then().statusCode(HttpStatus.SC_UNAUTHORIZED);
+    }
+
+    @Test
+    public void testPongIsAuthorized() {
+        given().auth().oauth2(createToken())
+                .when().get(pongEndpoint())
+                .then().statusCode(HttpStatus.SC_OK);
+    }
+
+    @Test
+    public void testPingPong() {
+        // When calling ping, the rest will invoke also the pong rest endpoint.
+        given()
+                .auth().oauth2(createToken())
+                .when().get(pingEndpoint())
+                .then().statusCode(HttpStatus.SC_OK)
+                .body(is("ping pong"));
+    }
+
+    protected abstract String endpointPrefix();
+
+    protected String pingEndpoint() {
+        return String.format(PING_ENDPOINT, endpointPrefix());
+    }
+
+    protected String pongEndpoint() {
+        return String.format(PONG_ENDPOINT, endpointPrefix());
+    }
+
+    private String createToken() {
+        return authzClient.obtainAccessToken(USER, USER).getToken();
+    }
+}
